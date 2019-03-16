c0acdd0abfcb49dc5e46f797690b0dd3a4d5ed03
029ye <370505634@qq.com>
2019-03-16 16:29:43 +0800

    创建自动部署shell脚本

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+#定义工程路径export PROJ_PATH=
+#定义Tomcat路径 export TOMCAT_PATH=
+#
+
+#定义关闭Tomcat 进程函数
+killTomcat(){
+   pid = `ps -ef | grep tomcat | grep java | awk '{print $2}'`
+   if [ "$pid" = "" ]
+   then
+     echo "no tomcat running"
+   else
+     kill -9 $pid
+   fi
+}
+
+cd $PROJ_PATH/jenkins-test
+mvn clean install
+
+#关闭Tomcat
+killTomcat
+
+rm -rf $TOMCAT_PATH/webapps/jenkins-test
+rm -f $TOMCAT_PATH/webapps/jenkins-test.war
+
+cp $PROJ_PATH/jenkins-test/target/jenkins-test.war $TOMCAT_PATH/webapps/
+cd $TOMCAT_PATH/
+sh bin/startup.sh
+
+
+
